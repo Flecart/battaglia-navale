78519905e054ff8666b7baecf3242d17c9ed3442
Angelo Huang <huangelo02@gmail.com>
2022-07-06 15:52:06 +0200

    refactor(api): add game middle link

--- requests/battleship/minitest.py
+++ requests/battleship/minitest.py
@@ -1,11 +1,11 @@
 from email.quoprimime import body_check
 import requests
 
-gameId = requests.get('http://localhost:3000/api/create').json()['gameId']
-userId1 = requests.post('http://localhost:3000/api/getid/' + gameId).json()['data']
-userId2 = requests.post('http://localhost:3000/api/getid/' + gameId).json()['data']
+gameId = requests.get('http://localhost:3000/api/game/create').json()['gameId']
+userId1 = requests.post('http://localhost:3000/api/game/getid/' + gameId).json()['data']
+userId2 = requests.post('http://localhost:3000/api/game/getid/' + gameId).json()['data']
 
-response = requests.post('http://localhost:3000/api/attack/' + gameId, json = {
+response = requests.post('http://localhost:3000/api/game/attack/' + gameId, json = {
     'player': userId1,
     'position': {
         "x": 9,
